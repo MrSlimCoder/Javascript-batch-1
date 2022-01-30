04c69a00a160095f514c3540fa39ab1ff421f5c1
viveksharmaui <sharma_vivek62@yahoo.com>
2022-01-30 18:40:23 +0500

    class 5

---
+++ 5/side.js
@@ -0,0 +1,32 @@
+// console.log("hello world 1");
+
+// setTimeout(() => console.log("hello word 2"), 0); //async
+
+// console.log("hello world 3");
+
+/* Data structures - Programming language k bd data structures */
+
+// for(let i=0; i<5; i++) {
+//     console.log(i);
+// } // O(n)
+
+// let myJson = {
+//     "123": {
+//         "name": "John",
+//         "age":30
+//     },
+//     "456": {
+//         "name": "Max",
+//         "age":25
+//     }
+// }
+
+// console.log(myJson["123"]) // O(1);
+
+
+// for (let i = 0; i < 100; i++) {
+//     // console.log(i);
+//     for (let j = 0; j < 100; j++) {
+//         // console.log(i);
+//     }
+// } // O(n^2)
