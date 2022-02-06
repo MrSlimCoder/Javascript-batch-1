b7bd924fbc92156363b0eb0ad5d682cc114c6f0f
viveksharmaui <sharma_vivek62@yahoo.com>
2022-02-06 17:45:38 +0500

    ilsa's issue

--- 7/script.js
+++ 7/script.js
@@ -1,4 +1,16 @@
+let parentDiv;
+
+window.onload = () => {
+    //write your code here
+    parentDiv = document.createElement('div');
+    parentDiv.id = "container";
+    document.body.appendChild(parentDiv);
+}
+
 for (let i = 1; i <= 5; i++) {
+    let childDiv = document.createElement('div');
+    let p = document.createElement('p');
+    p.innerHTML = "hello world";
     let img = document.createElement('img');
     fetch(`https://picsum.photos/200/300?random=1`, {
         method: 'GET', // *GET, POST, PUT, DELETE, etc.
@@ -6,13 +18,15 @@ for (let i = 1; i <= 5; i++) {
             'Content-Type': 'application/json'
         }
     }).then((response) => {
+        childDiv.id = `div - ${i}`;
         img.id = i
         img.src = response.url;
-        document.getElementById("images").appendChild(img);
+        childDiv.appendChild(p);
+        childDiv.appendChild(img);
+        parentDiv.appendChild(childDiv);
     }).catch((error) => {
         console.log("error", error);
     });
-
 }
 
 // GET - Return data
